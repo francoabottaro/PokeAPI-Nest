4e1a1ad535504c117f33eda97bad053b36e7887d
Franco Bottaro <francoabottaro@gmail.com>
2024-07-05 23:46:03 -0300

    publi + nest; intro api pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
-
 @Module({
-  imports: [PokemonModule],
-  controllers: [],
-  providers: []
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    PokemonModule
+  ]
 })
 export class AppModule {}
