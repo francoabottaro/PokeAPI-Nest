4b0c63d053f690de75568676f1fef393fe211f66
Franco Bottaro <francoabottaro@gmail.com>
2024-07-08 16:24:17 -0300

    Error database -> handleExpetions

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -23,15 +23,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`
-        );
-      }
-      console.log(error);
-      throw new InternalServerErrorException(
-        "Can't create Pokemon - Check server logs"
-      );
+      this.handleExceptions(error);
     }
   }
 
@@ -78,19 +70,23 @@ export class PokemonService {
       await pokemon.updateOne(updatePokemonDto);
       return { ...pokemon.toJSON(), ...updatePokemonDto };
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`
-        );
-      }
-      console.log(error);
-      throw new InternalServerErrorException(
-        "Can't update Pokemon - Check server logs"
-      );
+      this.handleExceptions(error);
     }
   }
 
   remove(id: string) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exists in db ${JSON.stringify(error.keyValue)}`
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      "Can't update Pokemon - Check server logs"
+    );
   }
 }
