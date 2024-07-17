20af80711e60dd3634733d9f28080b9807df9289
Franco Bottaro <francoabottaro@gmail.com>
2024-07-17 16:03:28 -0300

    seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = this.axios;
+  private readonly axios: AxiosInstance = axios;
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
